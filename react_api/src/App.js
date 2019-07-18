40ef355cd48c6012c528b04fb60cd43b045dbe48
windam <windam@live>
2019-07-18 17:19:14 +0530

    semantic ui

--- react_api/src/App.js
+++ react_api/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Card, Segment, Dimmer, Loader } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 const url = "https://jsonplaceholder.typicode.com/users";
@@ -24,19 +26,31 @@ class App extends React.Component {
     const { isLoaded, items } = this.state;
 
     if (!isLoaded) {
-      return <div>Loading ...</div>;
+      return (
+        <Dimmer active>
+          <Loader content='Loading' />
+        </Dimmer>
+      );
     } else {
       return (
         <div className='App'>
-          <ul>
-            {items.map(item => {
-              return (
-                <li key={item.id}>
-                  {item.name}| {item.email}
-                </li>
-              );
-            })}
-          </ul>
+          <Segment>
+            <Card.Group itemsPerRow={4}>
+              {items.map(item => {
+                return (
+                  <Card
+                    color='red'
+                    key={item.id}
+                    header={`${item.name} ${" "} ${item.email}`}
+                    meta={item.username}
+                    description={`Address: ${item.address.street} ${
+                      item.address.suite
+                    }`}
+                  />
+                );
+              })}
+            </Card.Group>
+          </Segment>
         </div>
       );
     }
