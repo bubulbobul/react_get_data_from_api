665ea597ab3244590ed926e366f12c6658c963fb
windam <windam@live>
2019-07-18 16:57:08 +0530

    fetching data from api

--- react_api/src/App.js
+++ react_api/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://jsonplaceholder.typicode.com/users";
+
+class App extends React.Component {
+  state = {
+    items: [],
+    isLoaded: false
+  };
+
+  componentDidMount() {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
+  }
+
+  render() {
+    const { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading ...</div>;
+    } else {
+      return (
+        <div className='App'>
+          <ul>
+            {items.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.name}| {item.email}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
